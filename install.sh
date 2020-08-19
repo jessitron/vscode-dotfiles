782600b86fbddd6228db3ca3f0b7785dd28f9914
Jessica Kerr <jessitron@gmail.com>
2020-08-19 16:48:34 -0500

    fail

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 cp ./.bash_profile ~/.bash_profile
 
 # maybe I should replace .bashrc entirely
-echo ". ~/.bash_profile" >> .bashrc
+echo ". ~/.bash_profile" >> ~/.bashrc
 
 echo "Hi Jess, I ran your dotfiles install script"
