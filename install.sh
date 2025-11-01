0ec6384563aa218ba0ae5c75c4281152a9dfcb94
Jessica Kerr <jessitron@gmail.com>
2025-11-01 15:15:54 -0500

    commit dangit

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cp ./.bash_profile ~/.bash_profile
 
+git config --global core.excludesfile ~/dotfiles/.gitignore_global
+
 # maybe I should replace .bashrc entirely
 echo ". ~/.bash_profile" >> ~/.bashrc
 
